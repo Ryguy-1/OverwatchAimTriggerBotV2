725b600b1a34032b3cf483f1f32c9d95175e53ec
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2021-11-07 17:54:53 -0800

    flickering being experienced...

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from numpy import character
-from screen import TriggerBot
+from screen import TriggerBot, Window
 import cv2
 import time
 from mss import mss
 import numpy as np
 
+
 def main():
-    trigger_bot = TriggerBot(resolution=(1920, 1080), snap_square_length=2, fire_delay=0.20)
+    ui = Window(uses_trigger_bot=True, uses_ammo_analyzer=True, time_between_frames_ms=1)
     
-def get_frame():
-    monitor = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
-    with mss() as sct:
-        # Get Screen Frame
-        frame = np.array(sct.grab(monitor))
-    return frame
-
 if __name__ == '__main__':
-
     main()
