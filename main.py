0562b184e82652018edbc11a62aac047c72f9ed7
Ryguy-1 <ryland.birchmeier.22@bishops.com>
2021-11-09 10:31:27 -0800

    Added Comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from screen import Window
 from trainer import GatherData
 from trainer import Train
 
-
+# Sets up Main Window
 def main():
     ui = Window(uses_trigger_bot=True, uses_ammo_analyzer=True, uses_flashbang_analyzer=True, time_between_frames_ms=1)
     
